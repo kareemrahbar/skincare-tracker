0e0dc59372321c09c8c05f726f161509f98f98fe
kareemrahbar <63873678+kareemrahbar@users.noreply.github.com>
2026-09-08 14:51:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 // Security headers - Fix CSP and allow app to work
 app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;");
+    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;");
     next();
 });
 
